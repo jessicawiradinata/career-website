7cfda8b8183b74c13fa251a8e9f32150190a8569
jessicawiradinata <jessicawiradinata@gmail.com>
2017-09-16 20:38:31 +1000

    Login Layout

--- src/containers/login/Layout.js
+++ src/containers/login/Layout.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
+import { FlatButton, ToolbarGroup, Toolbar, ToolbarTitle, Paper, TextField, RaisedButton } from 'material-ui'
 
 export default class LoginLayout extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
   render() {
     return (
-      <h1>Hello</h1>
+      <Paper style={styles.loginContainer} zDepth={1}>
+        <h1>Login</h1>
+        <TextField 
+          floatingLabelText="Email" 
+          style={styles.textField}
+          onChange={(email) => this.setState({ email: email.target.value })}
+        />
+        <TextField 
+          floatingLabelText="Password" 
+          type="password" 
+          style={styles.textField}
+          onChange={(password) => this.setState({ password: password.target.value })}
+        />
+        <RaisedButton 
+          label="Submit" 
+          primary={true} 
+          style={styles.submitBtn} 
+        />
+      </Paper>
     )
+  }
+}
+
+const styles = {
+  loginContainer: {
+      margin: 20,
+      padding: 40,
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center'
+  },
+  textField: {
+      width: '40%',
+  },
+  submitBtn: {
+      marginTop: 40,
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createBrowserHistory } from 'history'
-import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import Routes from './routes/index'
 import { ConnectedRouter } from 'react-router-redux'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 const history = createBrowserHistory()
 const store = configureStore({}, history)
 
 const App = () => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <Routes />
-      </div>
-    </ConnectedRouter>
-  </Provider>
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <Routes />
+        </div>
+      </ConnectedRouter>
+    </Provider>
+  </MuiThemeProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
