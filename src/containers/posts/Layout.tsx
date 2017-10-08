c1d69e0ade5df1b3e944021959b4f24dd1228aa6
jessicawiradinata <jessicawiradinata@gmail.com>
2017-10-08 18:26:00 +1100

    Extracted post cards on myposts and internships page as a component

--- src/containers/posts/Layout.tsx
+++ src/containers/posts/Layout.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import { Card, CardTitle, CardText, FontIcon, Chip, CardActions, FlatButton } from 'material-ui'
 import { map } from 'lodash'
 import Header from '../../components/Header'
-import moment from 'moment'
 import { History } from 'history'
 import { Post } from '../../domain/model/Post'
 import { User } from '../../domain/model/User'
+import PostCard from '../../components/PostCard'
 
 interface Props {
   posts: Post[]
@@ -18,74 +17,20 @@ interface Props {
 interface State {}
 
 export default class PostsLayout extends Component<Props, State> {
-  renderSkillsData = (skills: string[]) => {
-    return map(skills, (skill) => {
-      return (
-        <Chip
-          key={skill}
-          style={{ margin: 5 }}
-          onClick={() => null}>
-          {skill}
-        </Chip>
-      )
-    })
-  }
-
-  renderData = () => {
+  renderPostCards = () => {
     const { posts, user, history, deletePost } = this.props
     const isAdmin = user ? user.isAdmin : false
 
     return map(posts, (post: any) => {
-      const closingDate = moment(post.closingDate).format('DD MMM YYYY')
-      const postDate = moment(post.createdAt).format('DD MMM YYYY')
       return (
-        <Card style={styles.postCard} key={post._id}>
-          <CardTitle actAsExpander showExpandableButton>
-            <div style={styles.titleContainer as any}>
-              <h2>{post.title}</h2>
-              <div style={styles.marginRight50}>
-                <div style={styles.endContainer as any}>
-                  <FontIcon style={styles.marginRight5} className='material-icons'>location_on</FontIcon>
-                  <text>{post.location}</text>
-                </div>
-              </div>
-            </div>
-            <div style={styles.authorContainer as any}>
-              <text>{`Posted by ${post.authorName}`}</text>
-              <div style={styles.marginRight50}>
-                <div style={styles.endContainer as any}>
-                  <text>{post.remuneration}</text>
-                </div>
-              </div>
-            </div>
-          </CardTitle>
-          <CardText expandable>
-            <div style={styles.worktypeContainer as any}>
-              <text style={styles.marginRight5}>{`${post.workType} work`}</text>
-              <text style={styles.marginRight50}>{`Posted on ${postDate}`}</text>
-            </div>
-            <div style={styles.descriptionContainer as any}>
-              <h4>Description</h4>
-              <text>{post.description}</text>
-            </div>
-            <div style={styles.howContainer as any}>
-              <h4>How to Apply</h4>
-              <text>{post.howToApply}</text>
-            </div>
-          </CardText>
-          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-            <div style={{ display: 'flex', flexWrap: 'wrap', marginLeft: 8, marginTop: 5 }}>
-              {this.renderSkillsData(post.skills)}
-            </div>
-            <text style={{ marginRight: 50 }}>{`Closing on ${closingDate}`}</text>
-          </div>
-          { isAdmin &&
-            <CardActions style={styles.cardContainer as any}>
-              <FlatButton label='Edit' primary onClick={() => history.push(`/editpost/${post._id}`)} />
-              <FlatButton label='Delete' secondary onClick={() => deletePost(post._id)} />
-            </CardActions>
-          }
-        </Card>
+        <PostCard
+          key={post._id}
+          post={post}
+          history={history}
+          onDelete={deletePost}
+          showActions={isAdmin}
+          authorName={post.authorName}
+        />
       )
     })
   }
@@ -98,72 +43,8 @@ export default class PostsLayout extends Component<Props, State> {
     return (
       <div>
         <Header history={history} isLoggedIn={isLoggedIn} logout={logout} isAdmin={isAdmin} />
-        {this.renderData()}
+        {this.renderPostCards()}
       </div>
     )
   }
-}
-
-const styles = {
-  postCard: {
-    marginTop: 20,
-    paddingLeft: 10,
-    paddingBottom: 20,
-  },
-
-  endContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-
-  marginRight5: {
-    marginRight: 5,
-  },
-
-  marginRight50: {
-    marginRight: 50,
-  },
-
-  worktypeContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-
-  descriptionContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    marginBottom: 20,
-  },
-
-  howContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    marginBottom: 15,
-  },
-
-  titleContainer: {
-    display: 'flex',
-    alignItems: 'baseline',
-    justifyContent: 'space-between',
-  },
-
-  authorContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-
-  cardContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginRight: 30,
-  },
 }
