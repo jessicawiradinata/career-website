02d7ec59f3f602c05033ab6fe42f630795f26155
jessicawiradinata <jessicawiradinata@gmail.com>
2017-10-13 07:24:36 +1100

    Comments on all components under createpost

--- src/containers/createPost/Action.ts
+++ src/containers/createPost/Action.ts
@@ -1,9 +1,17 @@
+/**
+ * A collection of actions for create post page
+ */
 import * as ActionTypes from '../../constants/ActionTypes'
 import { History } from 'history'
 import { Dispatch } from 'redux'
 import { Post } from '../../domain/model/Post'
 import { dataLoadService } from '../../index'
 
+/**
+ * Creates a new post
+ * @param post new post to be created
+ * @param history navigation
+ */
 export const createPost = (post: Post, history: History) => (dispatch: Dispatch<any>) => (async () => {
   dispatch(createPostRequested())
   try {
@@ -15,14 +23,23 @@ export const createPost = (post: Post, history: History) => (dispatch: Dispatch<
   }
 })()
 
+/**
+ * Notifies that create post has been requested
+ */
 export const createPostRequested = () => {
   return { type: ActionTypes.CREATE_POST_REQUESTED }
 }
 
+/**
+ * Notifies that create post has been successful
+ */
 export const createPostSuccess = () => {
   return { type: ActionTypes.CREATE_POST_SUCCESS }
 }
 
+/**
+ * Notifies that create post has failed
+ */
 export const createPostFailed = () => {
   return { type: ActionTypes.CREATE_POST_FAILED }
 }
--- src/containers/createPost/selector.ts
+++ src/containers/createPost/selector.ts
@@ -2,14 +2,24 @@ import { createStructuredSelector } from 'reselect'
 import { State } from '../../store/State'
 import { User } from '../../domain/model/User'
 
+/**
+ * Props that will be used by Create Post page and their types
+ */
 interface StateProps {
   createPostStatus: any
   user: User
 }
 
+/**
+ * Takes states from the redux store and maps it to be a props for Create Post page
+ * @param state state from redux store
+ */
 const createPostStatus = (state: State) => state.post.createPostStatus
 const user = (state: State) => state.user.user
 
+/**
+ * Exports props to be used by Create Post page
+ */
 export default createStructuredSelector<State, StateProps>({
   createPostStatus,
   user,
