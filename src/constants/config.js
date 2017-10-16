3eb2cff8b2aac0e61d32e8ef1f747afff2903689
jessicawiradinata <jessicawiradinata@gmail.com>
2017-10-16 15:36:32 +1100

    Deleted unnecessary images, changes server destination to aws, reordered form in myaccount page

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,7 +2,7 @@
  * Specifies configurations for this app
  */
 
-export const API_ENDPOINT = 'http://192.168.1.15:8080/api'
+export const API_ENDPOINT = 'http://ec2-18-220-149-76.us-east-2.compute.amazonaws.com:8080/api'
 export const HEADER = {
   headers: {
     'token': window.localStorage.token
